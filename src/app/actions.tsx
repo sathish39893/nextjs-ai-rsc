62afad2f99e2436ff21b2cd211363bcfae99285e
Sathish Panthagani <sathish39893@gmail.com>
2024-06-02 15:30:05 +0530

    updated with server action

---
+++ src/app/actions.tsx
@@ -0,0 +1,15 @@
+'use server';
+
+import { openai } from '@ai-sdk/openai';
+import { CoreMessage, streamText } from 'ai';
+import { createStreamableValue } from 'ai/rsc';
+
+export async function continueConversation(messages: CoreMessage[]) {
+  const result = await streamText({
+    model: openai('gpt-4-turbo'),
+    messages,
+  });
+
+  const stream = createStreamableValue(result.textStream);
+  return stream.value;
+}
